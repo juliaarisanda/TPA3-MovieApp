11ad56dacd67a5123e868efdf4ff466878efa8ff
juliaarisanda <juliaarisanda12@gmail.com>
2022-10-13 16:32:41 +0700

    update movie app

--- movie.js
+++ movie.js
@@ -81,116 +81,14 @@ function showMovie(data) {
                 <h3>Overview</h3>
                 ${overview}
                 <br/> 
-                <button class="knowMore" id="${id}">Know More</button
             </div>
         
         `
         main.appendChild(movieEl);
 
-        document.getElementById(id).addEventListener('click', () => {
-          console.log(id)
-          openNav(movie)
-        })
     })
 }
-// Menampilkan overview video Film
-const overlayContent = document.getElementById('overlay-content');
-function openNav(movie) {
-  let id = movie.id;
-  fetch(BASE_URL + '/movie/'+id+'/videos?'+API_KEY).then(res => res.json()).then(videoData => {
-    console.log(videoData);
-    if(videoData){
-      document.getElementById("modalMovie").style.width = "100%";
-      if(videoData.results.length > 0){
-        var embed = [];
-        var dots = [];
-        videoData.results.forEach((video, idx) => {
-          let {name, key, site} = video
-          if(site == 'YouTube'){
-              
-            embed.push(`
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          
-          `)
-            dots.push(`
-              <span class="dot">${idx + 1}</span>
-            `)
-          }
-        })
-        
-        var content = `
-        <h1 class="no-results">${movie.original_title}</h1>
-        <br/>
-        
-        ${embed.join('')}
-        <br/>
 
-        <div class="dots">${dots.join('')}</div>
-        
-        `
-        overlayContent.innerHTML = content;
-        activeSlide=0;
-        showVideo();
-      }else{
-        overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
-      }
-    }
-  })
-}
-
-//Menutup tampilan overview video
-function closeNav() {
-  document.getElementById("modalMovie").style.width = "0%";
-}
-
-var activeSlide = 0;
-var totalVideos = 0;
-//Menampilkan video
-function showVideo(){
-  let embedClasses = document.querySelectorAll('.embed');
-  let dots = document.querySelectorAll('.dot');
-
-  totalVideos = embedClasses.length; 
-  embedClasses.forEach((embedTag, idx) => {
-    if(activeSlide == idx){
-      embedTag.classList.add('show')
-      embedTag.classList.remove('hide')
-
-    }else{
-      embedTag.classList.add('hide');
-      embedTag.classList.remove('show')
-    }
-  })
-
-  dots.forEach((dot, indx) => {
-    if(activeSlide == indx){
-      dot.classList.add('active');
-    }else{
-      dot.classList.remove('active')
-    }
-  })
-}
-// Menampilkan next prev video
-const leftArrow = document.getElementById('left-arrow')
-const rightArrow = document.getElementById('right-arrow')
-
-leftArrow.addEventListener('click', () => {
-  if(activeSlide > 0){
-    activeSlide--;
-  }else{
-    activeSlide = totalVideos -1;
-  }
-
-  showVideo()
-})
-rightArrow.addEventListener('click', () => {
-  if(activeSlide < (totalVideos -1)){
-    activeSlide++;
-  }else{
-    activeSlide = 0;
-  }
-  showVideo()
-})
 function getColor(vote) {
     if(vote>= 8){
         return 'green'
